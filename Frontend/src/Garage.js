28574d750615f3ae32e42011ed3fa893848d1619
fkhan000 <fakharyarkhan843@gmail.com>
2024-05-03 11:07:44 -0400

    sylized garage page

--- Frontend/src/Garage.js
+++ Frontend/src/Garage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-
+import backgroundImage from './Garage.jpg';
+import './Garage.css';
 const CarsDisplay = () => {
     const [cars, setCars] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -32,20 +33,29 @@ const CarsDisplay = () => {
     // Display content
     return (
         <div className="garage-container">
-            {cars.length > 0 && (
-                <div className="car-display">
-                    <button className="navigate-button left" onClick={handlePrev}>←</button>
-                    <img src={cars[currentIndex].image_path} alt={cars[currentIndex].name} className="car-image" />
-                    <button className="navigate-button right" onClick={handleNext}>→</button>
-                    <div className="car-info">
-                        <h2>{cars[currentIndex].name}</h2>
-                        <p>Model: {cars[currentIndex].model}</p>
-                        <p>Location: {cars[currentIndex].location || 'Not Currently Parked'}</p>
-                        <button className="select-button" onClick={handleSelectCar}>Select This Car</button>
-                    </div>
-                </div>
-            )}
+    {cars.length > 0 && (
+        <div className="car-display">
+            <div className="image-container">
+                <img src={cars[currentIndex].image_path} alt={cars[currentIndex].name} className="car-image" />
+            </div>
+            <div className="navigation-buttons">
+                <button className="navigate-button left" onClick={handlePrev}>←</button>
+                <button className="navigate-button right" onClick={handleNext}>→</button>
+            </div>
         </div>
+    )}
+    {cars.length > 0 && (
+        <div className="car-info">
+            <div className="car-info-box">
+                <h2>{cars[currentIndex].name}</h2>
+                <p>Model: {cars[currentIndex].model}</p>
+                <p>Location: {cars[currentIndex].location || 'Not Currently Parked'}</p>
+            </div>
+            <button className="select-button" onClick={handleSelectCar}>Select This Car</button>
+        </div>
+    )}
+</div>
+
     );
 };
 
