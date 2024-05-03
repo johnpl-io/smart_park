8603563c4a08e3a34b34aa045c30f6b7d9cb8f39
johnpl-io <john.nycjets@gmail.com>
2024-05-02 21:03:08 -0400

    added declarators

--- Backend/src/park_finder.py
+++ Backend/src/park_finder.py
@@ -17,25 +17,30 @@ from datetime import datetime, timedelta
 
 
 
-def park_find(user_id: int, spot_id: int, location: tuple[float, float]):
+def park_find(user_id: int, car_id: int, location: tuple[float, float])-> list[Spot, float, datetime]:
 
     session = create_session()
     """
 find a parking spot given a user and a specific location 
 a spot must have been recently left and close to the user
-    
+    Parameters:
+    :user_id: user_id of indivual requesting a spot. 
+    :car_id: the car_id of the user_id current car
+    :location: location of area where spot is
+
+    Returns:
+    :return: A tuple representing the latitude and longitude of the endpoint of the line segment represented by the car.
     """ 
 
-    #find closest points that have recently been left
+    #find closest points that have recently been left for now it simply picks ten closest poinst
     get_closest = (
-    session.query(Spot.spot_id, ParkHistory.time_left, func.ST_Distance(Spot.location,WKTElement(f'POINT({location[0]} {location[1]})')))
+    session.query(Spot, func.ST_Distance(Spot.location,WKTElement(f'POINT({location[0]} {location[1]})')),  ParkHistory.time_left)
     .join(ParkHistory, Spot.spot_id == ParkHistory.spot_id)
     .filter(ParkHistory.time_left >= datetime.now() - timedelta(days=10))
     .order_by(Spot.location.cast(Geometry).distance_centroid(WKTElement(f'POINT({location[0]} {location[1]})', srid=4326))
     
     ).limit(10)
     )
-
 
     return get_closest
 
